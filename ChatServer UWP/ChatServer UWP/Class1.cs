d8b543aae63ef920b2d9ee8a5c20bf1eab9ab669
Ali Ammar Alrawi <alialrawiu@gmail.com>
2017-02-07 09:24:47 +0100

    Chat server

---
+++ ChatServer UWP/ChatServer UWP/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer1 {
+    public static class Program {
+        static TcpListener server = null;
+        static IPAddress ip = IPAddress.Parse("");
+        static int port;
+        static List<TcpClient> clients = new List<TcpClient>();
+
+        public static void reciveSend() {
+            byte[] dataBytes = new byte[sizeof(int)];
+            string data;
+
+            while (true) {
+                TcpClient client = server.AcceptTcpClientAsync();
+
+                clients.Add(client);
+
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+
+                while ((i = stream.Read(dataBytes, 0, dataBytes.Length)) != 0) {
+
+                    data = Encoding.Unicode.GetString(dataBytes, 0, i);
+
+                }
+
+                foreach (TcpClient c in clients) {
+                    string newData = "";
+
+                    byte[] msg = Encoding.Unicode.GetBytes(newData);
+
+
+                    stream.Write(msg, 0, msg.Length);
+                }
+
+
+            }
+        }
+
+        public static void listener() {
+            try {
+                server = new TcpListener(ip, port);
+                server.Start();
+                reciveSend();
+
+            }
+            catch (SocketException e) {
+
+            }
+
+            finally {
+                server.Stop();
+            }
+        }
+
+        public static void Main(string[] args) {
+            listener();
+        }
+    }
+}
